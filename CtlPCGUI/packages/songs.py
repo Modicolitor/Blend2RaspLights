0c75dbd201bee611fc3f6da30845fc87942d4900
Modicolitor <Bjoern.Eckhardt@gmx.de>
2021-01-04 13:51:00 +0100

    multirasps verloren, wieder rein

--- CtlPCGUI/packages/songs.py
+++ CtlPCGUI/packages/songs.py
@@ -69,24 +69,29 @@ class Song(Songs):
     def playRasps(self):
         import paramiko
 
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(
-            paramiko.AutoAddPolicy())
+        sshs = []
+        ips = ["10.0.1.20", "10.0.1.20"]
+        for ip in ips:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(
+                paramiko.AutoAddPolicy())
 
-        # ssh.load_host_keys()
+            # ssh.load_host_keys()
 
-        # print(key)
-        ssh.connect("10.0.1.20",  username="pi",
-                    password="B!um3nBo+")  # password error
+            # print(key)
+
+            ssh.connect(ip,  username="pi",
+                        password="B!um3nBo+")  # password error
+            sshs.append(ssh)
         # ssh.start_client()
 
         # ssh_stdin, ssh_stouz, ssh_stderr = ssh.exec_command('cd /BLEND2BLINK/')
         # "ls -l")  # cd /BLEND2BLINK/")
-        ssh_stdin, ssh_stouz, ssh_stderr = ssh.exec_command(
-            'python json2blinkt-time.py')
-
-        ssh.close()
-        print("bambam lights on")
+        for ssh in sshs:
+            ssh_stdin, ssh_stouz, ssh_stderr = ssh.exec_command(
+                'python json2blinkt-time.py')
+            print("bambam lights on")
+            ssh.close()
 
     def playVideo(self):
         import cv2  # opencv
